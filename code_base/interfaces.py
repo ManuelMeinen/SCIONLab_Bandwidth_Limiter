a856757e86f4554b8d46657a1560f6ceb0c4350d
Manuel Meinen <m.meinen@gmx.ch>
2019-06-11 15:05:27 +0200

    debug mtu

--- code_base/interfaces.py
+++ code_base/interfaces.py
@@ -62,6 +62,7 @@ class Interface:
 
         for word in words:
             if word == 'mtu' or word[0:3] == 'MTU':
+                print(word)
                 if word == 'mtu':
                     return int(words[words.index(word)+1])
                 else:
